ee290e051dae314b28a65408b7a92efdea912641
tanuj-ralli <tanujralli97@gmail.com>
2022-09-17 20:03:26 +0530

    added wait_for_db management command

--- app/configs/settings.py
+++ app/configs/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tdd.apps.TddConfig',
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
